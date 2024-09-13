f3f1efcd0f94ee2abd01686514a7d8ff77d84852
Pierce <Pierce@PIERCE>
2024-09-13 12:55:56 -0400

    imported fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,12 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
+  useFonts({
+    "InriaSans-Light": require("./../assets/fonts/InriaSans-Light.ttf"),
+    "InriaSans-Regular": require("./../assets/fonts/InriaSans-Regular.ttf"),
+    "InriaSans-Bold": require("./../assets/fonts/InriaSans-Bold.ttf"),
+  });
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" />
