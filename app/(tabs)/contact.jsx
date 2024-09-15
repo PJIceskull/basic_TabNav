4e74cd6404cc0de53dc05774dbf953f5a29507bf
Pierce <Pierce@PIERCE>
2024-09-14 20:33:30 -0400

    add input boxes to contact page

--- app/(tabs)/contact.jsx
+++ app/(tabs)/contact.jsx
@@ -1,10 +1,62 @@
-import { View, Text } from "react-native";
+import { View, Text, SafeAreaView, TextInput, StyleSheet } from "react-native";
 import React from "react";
+import { Colors } from "../../constants/Colors";
 
 export default function contact() {
   return (
     <View>
-      <Text>Contact</Text>
+      <Text
+        style={{
+          fontFamily: "InriaSans-Bold",
+          fontSize: 48,
+          color: Colors.linkColor,
+        }}
+      >
+        Contact
+      </Text>
+      {/* <View>
+        <Text></Text>
+      </View> */}
+
+      <SafeAreaView>
+        <Text style={styles.textLabel}>First Name</Text>
+        <TextInput
+          placeholder="Enter Something"
+          style={styles.inputBox}
+        ></TextInput>
+        <Text style={styles.textLabel}>Last Name</Text>
+        <TextInput
+          placeholder="Enter Something"
+          style={styles.inputBox}
+        ></TextInput>
+        <Text style={styles.textLabel}>Email Address</Text>
+        <TextInput
+          placeholder="Enter Something"
+          style={styles.inputBox}
+        ></TextInput>
+        <Text style={styles.textLabel}>Phone Number</Text>
+        <TextInput
+          placeholder="Enter Something"
+          style={styles.inputBox}
+        ></TextInput>
+        <Text style={styles.textLabel}>Message</Text>
+        <TextInput
+          placeholder="Enter Something"
+          style={styles.inputBox}
+        ></TextInput>
+      </SafeAreaView>
     </View>
   );
 }
+const styles = StyleSheet.create({
+  inputBox: {
+    padding: 15,
+    borderColor: Colors.linkColor,
+    borderWidth: 2,
+    fontFamily: "InriaSans-Regular",
+  },
+  textLabel: {
+    fontFamily: "InriaSans-Bold",
+    fontSize: 24,
+  },
+});
