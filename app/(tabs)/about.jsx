0d6c413f02cad95ca56260f8d7cca5e9c6c43fd3
Pierce <Pierce@PIERCE>
2024-09-14 20:34:19 -0400

    add images and info for about page

--- app/(tabs)/about.jsx
+++ app/(tabs)/about.jsx
@@ -1,10 +1,56 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 import React from "react";
+import { Colors } from "../../constants/Colors";
 
 export default function about() {
   return (
     <View>
-      <Text>About</Text>
+      <Text
+        style={{
+          fontFamily: "InriaSans-Bold",
+          fontSize: 48,
+          color: Colors.linkColor,
+        }}
+      >
+        About Us
+      </Text>
+      <View style={styles.profileWrap}>
+        <Image
+          source={require("./../../assets/images/profile_pic/pexels-olly-3778876.jpg")}
+          style={styles.profilePic}
+          alt="Man in Black Suit Jacket Standing Near Railings"
+        ></Image>
+        <View></View>
+        <Text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+          officia soluta porro nam at laboriosam non, nostrum, harum illum
+          tempora repellat magni quis provident explicabo commodi tempore. Ut
+          deserunt quas, non dolores, veniam nam incidunt doloribus qui a ea
+          ratione inventore hic. Aliquid, ullam ipsa.
+        </Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 15,
+  },
+  profileWrap: {
+    maxWidth: 800,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    flexDirection: "row",
+    gap: 30,
+  },
+  profilePic: {
+    height: 250,
+    width: 250,
+    objectFit: "cover",
+    borderRadius: "50%",
+    borderColor: Colors.linkColor,
+    borderWidth: 5,
+  },
+});
