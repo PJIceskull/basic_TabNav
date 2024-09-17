e9e4403f4c2d341cc5fcf53d82caf32d78491868
Pierce <Pierce@PIERCE>
2024-09-17 19:38:34 -0400

    add styles to contact page

--- app/(tabs)/contact.jsx
+++ app/(tabs)/contact.jsx
@@ -18,32 +18,44 @@ export default function contact() {
         <Text></Text>
       </View> */}
 
-      <SafeAreaView>
-        <Text style={styles.textLabel}>First Name</Text>
-        <TextInput
-          placeholder="Enter Something"
-          style={styles.inputBox}
-        ></TextInput>
-        <Text style={styles.textLabel}>Last Name</Text>
-        <TextInput
-          placeholder="Enter Something"
-          style={styles.inputBox}
-        ></TextInput>
-        <Text style={styles.textLabel}>Email Address</Text>
-        <TextInput
-          placeholder="Enter Something"
-          style={styles.inputBox}
-        ></TextInput>
-        <Text style={styles.textLabel}>Phone Number</Text>
-        <TextInput
-          placeholder="Enter Something"
-          style={styles.inputBox}
-        ></TextInput>
-        <Text style={styles.textLabel}>Message</Text>
-        <TextInput
-          placeholder="Enter Something"
-          style={styles.inputBox}
-        ></TextInput>
+      <SafeAreaView style={{ margin: "auto" }}>
+        <View style={styles.groupWrap}>
+          <View style={styles.inputWrap}>
+            <Text style={styles.textLabel}>First Name</Text>
+            <TextInput
+              placeholder="Enter Something"
+              style={styles.inputBox}
+            ></TextInput>
+          </View>
+          <View style={styles.inputWrap}>
+            <Text style={styles.textLabel}>Last Name</Text>
+            <TextInput
+              placeholder="Enter Something"
+              style={styles.inputBox}
+            ></TextInput>
+          </View>
+        </View>
+        <View style={styles.inputWrap}>
+          <Text style={styles.textLabel}>Email Address</Text>
+          <TextInput
+            placeholder="Enter Something"
+            style={styles.inputBox}
+          ></TextInput>
+        </View>
+        <View style={styles.inputWrap}>
+          <Text style={styles.textLabel}>Phone Number</Text>
+          <TextInput
+            placeholder="Enter Something"
+            style={styles.inputBox}
+          ></TextInput>
+        </View>
+        <View style={styles.inputWrap}>
+          <Text style={styles.textLabel}>Message</Text>
+          <TextInput
+            placeholder="Enter Something"
+            style={styles.inputBox}
+          ></TextInput>
+        </View>
       </SafeAreaView>
     </View>
   );
@@ -54,9 +66,26 @@ const styles = StyleSheet.create({
     borderColor: Colors.linkColor,
     borderWidth: 2,
     fontFamily: "InriaSans-Regular",
+    fontSize: 16,
+    backgroundColor: "#fff",
+    marginBottom: 16,
+    // maxWidth: 600,
   },
   textLabel: {
     fontFamily: "InriaSans-Bold",
     fontSize: 24,
+    marginBottom: 8,
+  },
+  groupWrap: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    flexDirection: "row",
+    // maxWidth: 600,
+    gap: 20,
+  },
+  inputWrap: {
+    flex: 1,
+    // width: "40%",
   },
 });
